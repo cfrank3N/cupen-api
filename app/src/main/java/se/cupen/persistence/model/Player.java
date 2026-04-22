13d76fe4e824254e35233369eed8ff01649f61f2
Adam Frank <adamcecarfrank@gmail.com>
2026-04-22 12:44:17 +0200

    Added player and new folders

---
+++ app/src/main/java/se/cupen/persistence/model/Player.java
@@ -0,0 +1,22 @@
+package se.cupen.persistence.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Player {
+
+  private String name;
+  private String city;
+  private long pricemoney;
+
+}
