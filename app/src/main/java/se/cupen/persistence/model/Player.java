9eb2d16df8710904571a3099803ee5ac920cf2f0
Adam Frank <adamcecarfrank@gmail.com>
2026-04-22 12:56:51 +0200

    Added id to player

--- app/src/main/java/se/cupen/persistence/model/Player.java
+++ app/src/main/java/se/cupen/persistence/model/Player.java
@@ -1,6 +1,9 @@
 package se.cupen.persistence.model;
 
+import java.util.UUID;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,8 +18,11 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Player {
 
+  @GeneratedValue
+  private UUID id;
   private String name;
   private String city;
   private long pricemoney;
+  private Integer rating;
 
 }
