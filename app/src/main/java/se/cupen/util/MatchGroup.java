1a978d26510dc94aef90195be4b77c304c84b4cd
Adam Frank <adamcecarfrank@gmail.com>
2026-04-22 17:38:18 +0200

    Added enums to separate what game it is

---
+++ app/src/main/java/se/cupen/util/MatchGroup.java
@@ -0,0 +1,10 @@
+package se.cupen.util;
+
+public enum MatchGroup {
+  GROUP_A,
+  GROUP_B,
+  GROUP_C,
+  GROUP_D,
+  GROUP_E,
+  GROUP_F
+}
---
+++ app/src/main/java/se/cupen/util/MatchType.java
@@ -0,0 +1,12 @@
+package se.cupen.util;
+
+public enum MatchType {
+  GROUP_STAGE,
+  ROUND_OF_16,
+  QUARTER_FINAL,
+  SEMI_FINAL,
+  FINAL,
+  LOSERS_GAME,
+  SHOWMATCH,
+  THIRD_PLACE_GAME
+}
