3a8e9801fbb9246035fda649bae50f71749772f3
Adam Frank <adamcecarfrank@gmail.com>
2026-04-30 14:07:20 +0200

    Can add matches now

--- app/src/main/java/se/cupen/controller/MatchController.java
+++ app/src/main/java/se/cupen/controller/MatchController.java
@@ -2,13 +2,20 @@ package se.cupen.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpStatus;
+
 import se.cupen.dto.MatchDTO;
+import se.cupen.dto.creation.CreateMatch;
 import se.cupen.service.MatchService;
+import se.cupen.util.ResponseData;
 
 @RestController
 @RequestMapping("/api")
@@ -23,5 +30,11 @@ public class MatchController {
   @GetMapping("/matches")
   public ResponseEntity<List<MatchDTO>> fetchAllMatches() {
     return ResponseEntity.ok(matchService.findAllMatches());
+  }
+
+  @PostMapping("/matches")
+  public ResponseEntity<ResponseData<List<MatchDTO>>> createMatches(@RequestBody List<CreateMatch> matches) {
+    ResponseData<List<MatchDTO>> response = matchService.createMatches(matches);
+    return ResponseEntity.status(HttpStatus.SC_CREATED).body(response);
   }
 }
